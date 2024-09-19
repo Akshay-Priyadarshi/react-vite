073fae999e0d45eeba9df10b46798606cf435acc
Akshay Priyadarshi <akshayp1904@outlook.com>
2024-09-19 15:38:58 +0530

    Added shadcn button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/button/button";
+
 function App() {
     return (
-        <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col justify-center items-center space-y-4 h-screen">
             <h1 className="text-5xl">Hello React</h1>
+            <Button variant={"default"} size={"lg"}>
+                Click Me!
+            </Button>
         </div>
     );
 }
