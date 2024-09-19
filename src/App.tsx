aa207e02732073df4f1d75a0e8c49d311774f333
Akshay Priyadarshi <akshayp1904@outlook.com>
2024-09-19 18:47:29 +0530

    Moved home page content to home.page

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import { Button } from "./components/ui/button/button"
-import { getUsers } from "./services/user.service"
-import { User } from "./models/user.model"
-import { useQuery } from "@tanstack/react-query"
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/home.page"
 
 function App() {
-  const getUsersQuery = useQuery({ queryKey: ["getUsers"], queryFn: getUsers })
-
   return (
-    <div className="flex flex-col justify-center items-center space-y-4 h-screen">
-      <h1 className="text-5xl">Hello React</h1>
-      <Button
-        variant={"default"}
-        size={"lg"}
-        onClick={() => getUsersQuery.refetch()}
-      >
-        Click Me to refetch data
-      </Button>
-      <ul>
-        {getUsersQuery.isSuccess &&
-          getUsersQuery.data.map((user: User) => {
-            return <li key={user.id}>{user.name}</li>
-          })}
-      </ul>
-    </div>
+    <Routes>
+      <Route path={"/"} Component={HomePage} />
+    </Routes>
   )
 }
 
