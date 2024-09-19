a7d6b3183f84f50e1f22d71f8517ded311da053c
Akshay Priyadarshi <akshayp1904@outlook.com>
2024-09-19 17:12:51 +0530

    Formatted to prettier specs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { Button } from "./components/ui/button/button";
+import { Button } from "./components/ui/button/button"
 
 function App() {
-    return (
-        <div className="flex flex-col justify-center items-center space-y-4 h-screen">
-            <h1 className="text-5xl">Hello React</h1>
-            <Button variant={"default"} size={"lg"}>
-                Click Me!
-            </Button>
-        </div>
-    );
+  return (
+    <div className="flex flex-col justify-center items-center space-y-4 h-screen">
+      <h1 className="text-5xl">Hello React</h1>
+      <Button variant={"default"} size={"lg"}>
+        Click Me!
+      </Button>
+    </div>
+  )
 }
 
-export default App;
+export default App
